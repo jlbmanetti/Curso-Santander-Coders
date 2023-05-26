b2ee7dd1c079547882012d0c0b3496b5de5c9152
jlbmanetti <jlbmanetti@gmail.com>
2023-05-26 00:22:26 -0300

    fim python

---
+++ 05.12_python/exercicios_2.py
@@ -0,0 +1,21 @@
+'''ex2
+cont = 0
+resultado = 0
+n = 1000
+
+while cont != n:
+
+    resultado = resultado + 1/(2**cont)
+
+    cont = cont + 1
+    
+print(resultado)
+print(cont)
+'''
+'''ex3
+for _ in [10]:
+    print("Olá, mundo!")
+'''
+'''
+A=-item, B=item, C=-2*item, D=2*item
+'''
